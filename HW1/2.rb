56e9f5e9f3bc2a1ad90261c8222be7846cc12921
Anton Serdytyi <serdytyi@MacBook-Air-Anton.local>
2022-10-23 21:23:44 +0300

    resolve next task

--- HW1/2.rb
+++ HW1/2.rb
@@ -1,1 +1,7 @@
-
+# Дан целочисленный массив. Необходимо вывести вначале его элементы с нечетными индексами, а затем - четными.																									
+arr = (1..10).map { |el| rand(100) * el }
+p "initial arr: #{arr}"
+p 'odd numbers'
+arr.each_with_index { |el, i| puts el if i.odd? }
+p 'even numbers'
+arr.each_with_index { |el, i| puts el if i.even? }
